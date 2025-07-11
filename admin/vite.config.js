3f25c1fa0b003212922d92caee9f27cd3073a63b
Aditya Jadhav <adityajadhav97349@gmail.com>
2025-07-11 11:23:41 +0530

    fix: set correct base path for Render deployment

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{port:5174}
+  plugins: [react(), tailwindcss()],
+  base: '/', // ✅ Add this line!
+  server: { port: 5174 }
 })
---
+++ backend/text.md
@@ -0,0 +1,5 @@
+https://healthmate-backend-gs8z.onrender.com
+
+https://healthmate-frontend-ksyq.onrender.com
+
+https://healthmate-admin-usjs.onrender.com
